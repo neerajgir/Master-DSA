aa630d17dfd57e636fae4cb3ffa893b5a0e23fc0
Neeraj Gir <neerajgoswami871@gmail.com>
2026-09-13 22:52:00 +0500

    Add prime number check functionality to loop-1.js

---
+++ DAY 04 LOOPS/loop-1.js
@@ -0,0 +1,92 @@
+// let n = Number(prompt("Enter a number:"));
+
+// for(let i = 1; i <= n; i++){
+//     console.log("hello world");
+// }
+
+// 1 to n
+// let n = Number(prompt("Enter a number:"));
+
+// for(let i = 1; i <= n; i++){
+//     console.log(i);
+//
+// }
+
+
+//n to 1
+
+// let n = Number(prompt("Enter a number:"));
+
+// for(let i = n; i >= 1; i--){
+//     console.log(i);
+
+// }
+
+
+//tables
+// let n = Number(prompt("Enter a number:"));
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(n*i);
+
+// }
+
+//sum up to n terms
+
+// let n = Number(prompt("Enter a number:"));
+// let sum = 0;
+// for(let i = 1; i <= n; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+//factorial of num
+// let n = Number(prompt("Enter a number:"));
+// let fact = 1;
+// for(let i = 1; i <= n; i++){
+//     fact = fact * i;
+// }
+// console.log(fact);
+
+//print sum of all even and odd num range separately
+// let start = Number(prompt("Enter start of range:"));
+// let end = Number(prompt("Enter end of range:"));
+// let sumEven = 0;
+// let sumOdd = 0;
+// for(let i = start; i <= end; i++){
+//     if(i % 2 === 0){
+//         sumEven = sumEven + i;
+//     } else {
+//         sumOdd = sumOdd + i;
+//     }
+// }
+// console.log("Sum of even numbers:", sumEven);
+// console.log("Sum of odd numbers:", sumOdd);
+
+//print all the factors of a number
+// let n = Number(prompt("Enter a number:"));
+
+// for(let i = 1; i <= n; i++){
+//     if(n % i === 0){
+//         console.log(i);
+//     }
+// }
+
+//check the num of prime or not
+let n = Number(prompt("Enter a number:"));
+let isPrime = true;
+
+for(let i = 2; i < n/2; i++){
+    if(n % i === 0){
+        isPrime = false;
+        break;
+    }
+}
+if(isPrime){
+    console.log(n + " is a prime number.");
+} else {
+    console.log(n + " is not a prime number.");
+}
+
+
+
